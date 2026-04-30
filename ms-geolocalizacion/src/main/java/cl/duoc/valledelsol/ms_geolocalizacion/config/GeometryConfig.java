325eee3bdfe9f2972de0fcd2ad2da7ba03677693
Mirko <mirkoxd.314@gmail.com>
2026-04-29 20:36:56 -0400

    fix: integra PostGIS y listener en ms-geolocalizacion

---
+++ ms-geolocalizacion/src/main/java/cl/duoc/valledelsol/ms_geolocalizacion/config/GeometryConfig.java
@@ -0,0 +1,15 @@
+package cl.duoc.valledelsol.ms_geolocalizacion.config;
+
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeometryConfig {
+
+    @Bean
+    GeometryFactory geometryFactory() {
+        return new GeometryFactory(new PrecisionModel(), 4326);
+    }
+}
--- ms-geolocalizacion/src/main/java/cl/duoc/valledelsol/ms_geolocalizacion/entity/FocoIncendio.java
+++ ms-geolocalizacion/src/main/java/cl/duoc/valledelsol/ms_geolocalizacion/entity/FocoIncendio.java
@@ -1,47 +1,31 @@
 package cl.duoc.valledelsol.ms_geolocalizacion.entity;
 
-import org.springframework.data.geo.Point;
-
-import cl.duoc.valledelsol.ms_geolocalizacion.enums.EstadoFoco;
-import cl.duoc.valledelsol.ms_reportes.entity.Reporte;
+import org.locationtech.jts.geom.Point;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "FocoIncendio") //Nombre de la tabla en la base de datos
-@Data //Creador automatico de getters y setters
-@AllArgsConstructor //Crea un constructor con todo los campos
-@NoArgsConstructor //Crea un constructor vacio para poder crear objetos
+@Table(name = "foco_incendio")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FocoIncendio {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "idReporte", nullable = false)
-    private Reporte reporteId;
+    @Column(name = "reporte_id")
+    private Long reporteId;
 
-    @Transient //Atributo que sirve para informar que la base de datos que este sera un dato no persistente
+    @Column(nullable = false, columnDefinition = "geometry(Point, 4326)")
     private Point ubicacion;
-
-    @Column(nullable = false)
-    private Double radioEstimado;
-
-    @Column(nullable = false)
-    private EstadoFoco estado;
-
-    @Column(nullable = false)
-    private Data fechaActualizacion;
 
 }
